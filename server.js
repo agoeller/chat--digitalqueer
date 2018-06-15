d0b4df5e05ea96740660bd6e791d0eb48f81710c
Anneli Goeller <agoeller@saic.edu>
2018-06-15 15:07:44 -0500

    Center items vertically

--- server.js
+++ server.js
@@ -11,33 +11,33 @@ app.use( express.static(__dirname+'/public') );
 //https://socket.io/get-started/chat/
 
 //SOCKET FUNCTIONS
-function isSocketAdmin(socket){
-  var adminURL = 'http://localhost:3000/admin.html';
-  if( socket.handshake.headers.referer == adminURL ){
-    return true;
-  } else {
-    return false;
-  }
-}
-
-io.on('connection', function(socket){
-  if( isSocketAdmin(socket) ) admin = socket;
-
-  //USER SENDING MESSAGE TO ADMIN
-  socket.on('user-message',function(msg){
-    var userMessage = { id:socket.id, message:msg };
-    admin.emit('user-message',userMessage);
-  });
-
-  //ADMIN SENDING MESSAGE TO USER
-  socket.on('admin-message',function(msg){
-    io.sockets.connected[msg.id].emit('admin-message',msg.message);
-  });
-
-  socket.on('disconnect',function(){
-    if( isSocketAdmin(socket) ) admin = null;
-  });
-});
+// function isSocketAdmin(socket){
+//   var adminURL = 'http://localhost:3000/admin.html';
+//   if( socket.handshake.headers.referer == adminURL ){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+//
+// io.on('connection', function(socket){
+//   if( isSocketAdmin(socket) ) admin = socket;
+//
+//   //USER SENDING MESSAGE TO ADMIN
+//   socket.on('user-message',function(msg){
+//     var userMessage = { id:socket.id, message:msg };
+//     admin.emit('user-message',userMessage);
+//   });
+//
+//   //ADMIN SENDING MESSAGE TO USER
+//   socket.on('admin-message',function(msg){
+//     io.sockets.connected[msg.id].emit('admin-message',msg.message);
+//   });
+//
+//   socket.on('disconnect',function(){
+//     if( isSocketAdmin(socket) ) admin = null;
+//   });
+// });
 
 
 //SERVER LISTENING ON PORT 3000
